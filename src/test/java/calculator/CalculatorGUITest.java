e9a6eae048d62f89253fb21f42eaedea71b13053
Цезарь <nexushonor29@gmail.com>
2023-08-02 21:35:50 +0300

    Calc3

--- src/test/java/calculator/CalculatorGUITest.java
+++ src/test/java/calculator/CalculatorGUITest.java
@@ -1,78 +1,72 @@
 package calculator;
 
-
+import java.awt.Component;
+import javax.swing.JButton;
+import javax.swing.SwingUtilities;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import javax.swing.*;
-import java.awt.*;
-import java.util.HashMap;
-import java.util.Map;
-
 public class CalculatorGUITest {
     private CalculatorGUI calculatorGUI;
-    private final Map<String, JButton> buttonsMap = new HashMap<>();
 
     @BeforeEach
     public void setUp() {
-        SwingUtilities.invokeLater(() -> {
-            calculatorGUI = new CalculatorGUI();
-            getButtonsFromPanel();
-        });
-    }
-
-    private void getButtonsFromPanel() {
-        for (Component component : calculatorGUI.getContentPane().getComponents()) {
-            if (component instanceof JPanel) {
-                JPanel buttonPanel = (JPanel) component;
-                for (Component buttonComponent : buttonPanel.getComponents()) {
-                    if (buttonComponent instanceof JButton) {
-                        JButton button = (JButton) buttonComponent;
-                        buttonsMap.put(button.getText(), button);
-                    }
-                }
-            }
-        }
+        calculatorGUI = new CalculatorGUI();
     }
 
     @Test
     public void testAdd() {
-        clickButtons("1 + 1 =");
-        Assertions.assertEquals("2", calculatorGUI.getDisplayText());
+        pressButtons("3 + 5 =");
+        Assertions.assertEquals("8", calculatorGUI.getDisplayText());
     }
 
     @Test
     public void testSubtract() {
-        clickButtons("7 - 3 =");
-        Assertions.assertEquals("4", calculatorGUI.getDisplayText());
+        pressButtons("10 - 4 =");
+        Assertions.assertEquals("6", calculatorGUI.getDisplayText());
     }
 
     @Test
     public void testMultiply() {
-        clickButtons("3 * 5 =");
-        Assertions.assertEquals("15", calculatorGUI.getDisplayText());
+        pressButtons("2 * 3 =");
+        Assertions.assertEquals("6", calculatorGUI.getDisplayText());
     }
 
     @Test
     public void testDivide() {
-        clickButtons("8 / 2 =");
-        Assertions.assertEquals("4", calculatorGUI.getDisplayText());
+        pressButtons("10 / 2 =");
+        Assertions.assertEquals("5", calculatorGUI.getDisplayText());
     }
 
     @Test
     public void testDivideByZero() {
-        clickButtons("5 / 0 =");
+        pressButtons("10 / 0 =");
         Assertions.assertEquals("Error", calculatorGUI.getDisplayText());
     }
 
-    private void clickButtons(String input) {
-        for (char c : input.toCharArray()) {
-            String label = String.valueOf(c);
-            JButton button = buttonsMap.get(label);
-            if (button != null) {
-                SwingUtilities.invokeLater(button::doClick);
+    private void pressButtons(String buttonSequence) {
+        for (char c : buttonSequence.toCharArray()) {
+            pressButton(c);
+        }
+    }
+
+    private void pressButton(char c) {
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        Component[] components = calculatorGUI.getContentPane().getComponents();
+        for (Component component : components) {
+            if (component instanceof JButton) {
+                JButton button = (JButton) component;
+                if (button.getText().equals(String.valueOf(c))) {
+                    SwingUtilities.invokeLater(() -> button.doClick());
+                    return;
+                }
             }
         }
     }
-}
+    }
